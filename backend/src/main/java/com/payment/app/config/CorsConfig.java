2a21b7285c54b68fb5c5ee85202099ea8a09031b
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-03-07 14:36:14 -0300

    add CORS config

---
+++ backend/src/main/java/com/payment/app/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.payment.app.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${frontend.port}")
+    private String frontendPort;
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                // Allow Frontend to access Backend
+                registry.addMapping("/**")
+                        .allowedOrigins(frontendUrl + ":" + frontendPort)
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*");                
+            }
+        };
+    }
+}

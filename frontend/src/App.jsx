c3479e657f4dd5392b1e7c926d84f689f8d847aa
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-03-07 14:54:40 -0300

    Add user list to test fe-be integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { getUsers } from "./services/api"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers().then(data => setUsers(data));
+  }, []);
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+            <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <div>
+          <h1>Usuário</h1>
+          <ul>
+            {users.map(user => (
+              <li key={user.id}>
+                {user.name} - {user.email}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <h1>Vite + React</h1>
       <div className="card">

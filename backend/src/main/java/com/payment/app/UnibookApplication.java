1393d83d99fda01174ed19273061d1b5f3da00ac
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-04-09 17:38:03 -0300

    update project label

--- backend/src/main/java/com/payment/app/UnibookApplication.java
+++ backend/src/main/java/com/payment/app/UnibookApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymanagerApplication {
+public class UnibookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymanagerApplication.class, args);
+		SpringApplication.run(UnibookApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/payment/app/PaymanagerApplicationTests.java
+++ backend/src/test/java/com/payment/app/PaymanagerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PaymanagerApplicationTests {
+class UnibookApplicationTests {
 
 	@Test
 	void contextLoads() {

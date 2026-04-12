f5204c2d525625e61c0e7edc7908ed313b368726
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-04-12 16:55:01 -0300

    Adding permission files

---
+++ backend/src/main/java/com/unibook/app/model/Permission.java
@@ -0,0 +1,31 @@
+package com.unibook.app.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "permissions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(name = "group_name")
+    private String groupName;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "permissions")
+    private List<Role> roles = new ArrayList<>();
+}

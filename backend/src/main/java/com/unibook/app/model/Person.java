7cf57194c6965bc38ed2eb91fe2f02cd291a40f7
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-04-12 16:54:48 -0300

    Adding Person Files

---
+++ backend/src/main/java/com/unibook/app/model/Person.java
@@ -0,0 +1,22 @@
+package com.unibook.app.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "persons")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+}

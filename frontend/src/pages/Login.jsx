b9883d8217142f32d6a29db31a87092be07d0384
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-04-14 23:59:23 -0300

    Adding basic login page to test front/back communication

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { loginRequest } from "../services/auth";
+import { useNavigate } from "react-router-dom";
+
+import Input from "../components/Input";
+import Button from "../components/Button";
+
+export default function Login() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { login: doLogin } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const data = await loginRequest(login, password);
+      doLogin(data.token);
+      navigate("/dashboard");
+    } catch (err) {
+      setError("Credenciais inválidas");
+    }
+  }
+
+  return (
+    <div style={{ maxWidth: 300, margin: "100px auto" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <Input
+          type="text"
+          placeholder="Login"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
+
+        <Input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button type="submit">Entrar</Button>
+      </form>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
+}

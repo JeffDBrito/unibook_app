3be254dd8a804690d8d3fc5f287762de8f69adeb
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-04-15 10:44:53 -0300

    Add config/env and import api_url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,9 @@
-const BASE_URL = "http://localhost:8080";
+import { ENV } from "../config/env";
+
+const BASE_URL = ENV.API_URL;
 
 export async function api(path, options = {}) {
+  console.log(BASE_URL);
   const token = localStorage.getItem("token");
 
   return fetch(`${BASE_URL}${path}`, {

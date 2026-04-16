bb4f3c8fc8dfdd29db7df4432cf12e1333236f64
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-04-16 16:46:23 -0300

    Improve routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import { PrivateRoute } from "./PrivateRoute";
+import Login from "../pages/Login";
+import Books from "../pages/Books";
+import Users from "../pages/Users";
+import Loans from "../pages/Loans";
 
 export default function AppRoutes() {
   return (
@@ -9,14 +12,11 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<Login />} />
 
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard title="Dashboard"/></PrivateRoute>} />
+        <Route path="/books" element={<PrivateRoute><Books title="Books"/></PrivateRoute>} />
+        <Route path="/loans" element={<PrivateRoute><Loans title="Loans"/></PrivateRoute>} />
+        <Route path="/users" element={<PrivateRoute><Users title="Users"/></PrivateRoute>} />
+
       </Routes>
     </BrowserRouter>
   );

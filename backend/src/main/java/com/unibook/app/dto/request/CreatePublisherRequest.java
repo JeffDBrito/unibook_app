4c1022110af86883b97fc4f26da80f066fcae500
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-04-20 23:07:22 -0300

    Adding CreatePublisherRequest

---
+++ backend/src/main/java/com/unibook/app/dto/request/CreatePublisherRequest.java
@@ -0,0 +1,14 @@
+package com.unibook.app.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreatePublisherRequest {
+    
+    private String title;
+
+    public CreatePublisherRequest() {}
+
+}

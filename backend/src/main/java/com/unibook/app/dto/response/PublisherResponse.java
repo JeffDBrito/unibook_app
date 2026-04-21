a4b2ab259220edab0e1c0b7059dc9a50f0c16a28
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-04-20 23:07:32 -0300

    Adding PublisherResponse

---
+++ backend/src/main/java/com/unibook/app/dto/response/PublisherResponse.java
@@ -0,0 +1,13 @@
+package com.unibook.app.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PublisherResponse {
+    
+    private Long id;
+    private String title;
+
+}

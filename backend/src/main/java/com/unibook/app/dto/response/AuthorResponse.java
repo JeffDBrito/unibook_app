30bcb2c15aeb6ae1490087f8ff10dabb2d5b98ac
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-04-20 23:06:09 -0300

    Adding AuthorResponse

---
+++ backend/src/main/java/com/unibook/app/dto/response/AuthorResponse.java
@@ -0,0 +1,14 @@
+package com.unibook.app.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthorResponse {
+    
+    private Long id;
+    private String name;
+    private String biography;
+
+}

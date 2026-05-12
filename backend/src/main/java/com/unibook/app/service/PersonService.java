649f5784871a4d66b593bb8242ebae32bb082361
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-12 06:22:10 -0300

    Creating Person Update Function

--- backend/src/main/java/com/unibook/app/service/PersonService.java
+++ backend/src/main/java/com/unibook/app/service/PersonService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.unibook.app.dto.request.person.UpdatePersonRequest;
 import com.unibook.app.dto.response.PersonResponse;
 import com.unibook.app.model.Person;
 import com.unibook.app.repository.PersonRepository;
@@ -57,7 +58,23 @@ public class PersonService {
     public void deleteById(Long id) {
         Person person = personRepository.findById(id)
             .orElseThrow(() -> new RuntimeException("Person not found with id: " + id));
-        personRepository.delete(person);
+        person.softDelete();
+        personRepository.save(person);
+    }
+
+    public PersonResponse update(Long id, UpdatePersonRequest request, boolean partial){
+        Person person = personRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Person not Found"));
+        
+        if(!partial || request.getName() != null){
+            person.setName(request.getName());
+        }
+
+        if(!partial || request.getEmail() != null){
+            person.setEmail(request.getEmail());
+        }
+
+        return toResponse(person);
     }
 
     // ----------------- //

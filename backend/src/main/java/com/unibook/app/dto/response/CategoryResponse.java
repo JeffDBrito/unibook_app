8a52c0b08209dc956d4c73ad90751d3a39391add
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-12 23:16:27 -0300

    Add Schema examples to CategoryResponse

--- backend/src/main/java/com/unibook/app/dto/response/CategoryResponse.java
+++ backend/src/main/java/com/unibook/app/dto/response/CategoryResponse.java
@@ -1,5 +1,6 @@
 package com.unibook.app.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +8,13 @@ import lombok.Setter;
 @Setter
 public class CategoryResponse {
 
+    @Schema(example = "1")
     private Long id;
+
+    @Schema(example = "Fiction")
     private String title;
+
+    @Schema(example = "Fiction is a literary genre that includes imaginative or invented stories, often featuring characters and events that are not based on real life.")
     private String description;
     
 }

1d8d7034f3a2d347839c1eacd23ade9e8b35a05e
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-12 23:17:22 -0300

    Add Schema examples to PersonResponse

--- backend/src/main/java/com/unibook/app/dto/response/PersonResponse.java
+++ backend/src/main/java/com/unibook/app/dto/response/PersonResponse.java
@@ -1,5 +1,6 @@
 package com.unibook.app.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,10 @@ import lombok.Setter;
 @Setter
 public class PersonResponse {
     
+    @Schema(example = "John Doe")
     private String name;
+    
+    @Schema(example = "john@email.com")
     private String email;
 
 }

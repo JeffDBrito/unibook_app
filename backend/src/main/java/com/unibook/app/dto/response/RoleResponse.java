66e923325e0f64570fee41d12891490c83471c57
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-12 23:17:51 -0300

    Add Schema examples to RoleResponse

--- backend/src/main/java/com/unibook/app/dto/response/RoleResponse.java
+++ backend/src/main/java/com/unibook/app/dto/response/RoleResponse.java
@@ -2,6 +2,7 @@ package com.unibook.app.dto.response;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,13 @@ import lombok.Setter;
 @Setter
 public class RoleResponse {
     
+    @Schema(example = "1")
+    private Long id;
+
+    @Schema(example = "Admin")
     private String name;
+    
+    @Schema(example = "[Create example, Read Example, Delete Example]")
     private List<String> permissions;
 
 }

9086928d281b6c0850405e73d4b823afff49ec63
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-12 23:15:51 -0300

    Add Schema examples to AuthorResponse

--- backend/src/main/java/com/unibook/app/dto/response/AuthorResponse.java
+++ backend/src/main/java/com/unibook/app/dto/response/AuthorResponse.java
@@ -1,5 +1,6 @@
 package com.unibook.app.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +8,13 @@ import lombok.Setter;
 @Setter
 public class AuthorResponse {
     
+    @Schema(example = "1")
     private Long id;
+
+    @Schema(example = "John Doe")
     private String name;
+    
+    @Schema(example = "John Doe is a renowned author known for his compelling storytelling.")
     private String biography;
 
 }

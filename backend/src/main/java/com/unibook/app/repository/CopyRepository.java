bb1d1fa04231e48bf97b2a86b01e6aea3a2763f4
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-13 21:33:51 -0300

    Creating CopyRepository

---
+++ backend/src/main/java/com/unibook/app/repository/CopyRepository.java
@@ -0,0 +1,9 @@
+package com.unibook.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unibook.app.model.Copy;
+
+public interface CopyRepository extends JpaRepository<Copy, Long>{
+    
+}

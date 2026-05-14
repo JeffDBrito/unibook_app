74aff8a30ef09361d950acd57a9133ed63c68e23
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-14 00:43:21 -0300

    Improve Fine Entity

--- backend/src/main/java/com/unibook/app/model/Fine.java
+++ backend/src/main/java/com/unibook/app/model/Fine.java
@@ -1,11 +1,17 @@
 package com.unibook.app.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import com.unibook.app.enums.FineStatus;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -23,11 +29,18 @@ public class Fine extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    private BigDecimal amount;
+    
+    private String reason;
+
+    private LocalDate issuedDate;
+
+    private LocalDate paidDate;
+
+    @Enumerated(EnumType.STRING)
+    private FineStatus status;
+    
     @OneToOne
+    @JoinColumn(name = "loan_id")
     private Loan loan;
-    
-    private float amount;
-    
-    private LocalDate paidAt;
-    
 }

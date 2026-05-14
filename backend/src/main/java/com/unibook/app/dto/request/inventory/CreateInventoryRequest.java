83cf7590ecadd48e30d52c95bfeda6bb51383397
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-13 21:34:44 -0300

    Create CreateInventoryRequest dto

---
+++ backend/src/main/java/com/unibook/app/dto/request/inventory/CreateInventoryRequest.java
@@ -0,0 +1,23 @@
+package com.unibook.app.dto.request.inventory;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateInventoryRequest {    
+    
+    @Schema(example = "A")
+    private String sector;
+    
+    @Schema(example = "2")
+    private String shelf;
+    
+    @Schema(example = "4")
+    private Integer row;
+    
+    @Schema(example = "12")
+    private Integer slot;
+
+}

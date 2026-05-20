01eea8dc02e474dd785480b5ea52bb5af11447c9
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-20 19:03:04 -0300

    Add validation to CreateUserRequest

--- backend/src/main/java/com/unibook/app/dto/request/user/CreateUserRequest.java
+++ backend/src/main/java/com/unibook/app/dto/request/user/CreateUserRequest.java
@@ -4,6 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,21 +18,30 @@ import lombok.Setter;
 @AllArgsConstructor
 public class CreateUserRequest {
 
+    @NotBlank(message = "Name is required")
     @Schema(example = "John Doe")
     private String name;
 
+    @Email(message = "Invalid email")
+    @NotBlank(message = "Email is required")
     @Schema(example = "john@email.com")
     private String email;
 
+    @Past(message = "Birth date must be in the past")
+    @NotNull(message = "Birth date is required")
     @Schema(example = "2000-12-24")
     private LocalDate birthDate;
 
+    @NotBlank(message = "Login is required")
+    @Size(min = 3, max = 15, message = "Login must contain between 3 and 15 characters")
     @Schema(example = "john123")
     private String login;
 
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must contain at least 6 characters")
     @Schema(example = "123456")
     private String password;
-
+    
     @Schema(example = "[1, 2]")
     private List<Long> roleIds;
 

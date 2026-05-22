32e79e7f84aac2eff8ce3c2301629dea5ad2f3ef
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-21 21:06:43 -0300

    Add unique email to person

--- backend/src/main/java/com/unibook/app/model/Person.java
+++ backend/src/main/java/com/unibook/app/model/Person.java
@@ -19,6 +19,8 @@ public class Person extends BaseEntity{
     private Long id;
 
     private String name;
+
+    @Column(unique = true)
     private String email;
     private LocalDate birthDate;
 

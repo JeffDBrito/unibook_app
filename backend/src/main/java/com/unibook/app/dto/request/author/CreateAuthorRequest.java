759934ada3e9ee7d0199acc0cef8bfbbfd50f6af
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-26 22:39:05 -0300

    Add Validation to Create Author Request

--- backend/src/main/java/com/unibook/app/dto/request/author/CreateAuthorRequest.java
+++ backend/src/main/java/com/unibook/app/dto/request/author/CreateAuthorRequest.java
@@ -3,6 +3,10 @@ package com.unibook.app.dto.request.author;
 import java.time.LocalDate;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,12 +16,18 @@ import lombok.Setter;
 @AllArgsConstructor
 public class CreateAuthorRequest {
     
+    @Size(min = 1, max = 100, message = "Name must contain between 1 and 100 characters")
+    @NotBlank(message = "Name is required")
     @Schema(example = "John Doe")
     private String name;
     
+    @Size(min = 1, max = 500, message = "Biography must contain between 1 and 500 characters")
+    @NotBlank(message = "Biography is required")
     @Schema(example = "John Doe is a renowned author known for his compelling storytelling.")
     private String biography;
 
+    @Past(message = "Birth date must be in the past")
+    @NotNull(message = "Birth date is required")
     @Schema(example = "2000-12-24")
     private LocalDate birthDate;
 

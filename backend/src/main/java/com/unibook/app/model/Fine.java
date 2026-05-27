736f4e686ca59e196ce4aae8281f3a5dbf888bed
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-26 22:11:36 -0300

    Remove unused import

--- backend/src/main/java/com/unibook/app/model/Fine.java
+++ backend/src/main/java/com/unibook/app/model/Fine.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 
 import com.unibook.app.enums.FineStatus;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;

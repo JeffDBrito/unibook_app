92ed5d16f531b8f68f13e37772db7c6905e4b562
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-27 22:40:47 -0300

    Add custom method to Status enums

--- backend/src/main/java/com/unibook/app/enums/CopyStatus.java
+++ backend/src/main/java/com/unibook/app/enums/CopyStatus.java
@@ -1,11 +1,27 @@
 package com.unibook.app.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum CopyStatus {
 
     RENTED,
     AVAILABLE,
     LOST,
     DAMAGED,
-    REASSIGNED
+    REASSIGNED;
+
+    @JsonCreator
+    public static CopyStatus fromValue(String value){
+
+        for(CopyStatus status : values()){
+            if(status.name().equalsIgnoreCase(value)){
+                return status;
+            }
+        }
+
+        throw new IllegalArgumentException(
+            "Invalid Copy status"
+        );
+    }
     
 }

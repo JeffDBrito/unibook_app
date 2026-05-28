c299cc0245cb1daf2e569dbf3d6ae60b49931051
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-28 17:00:43 -0300

    Add validations to CreateInventoryRequest

--- backend/src/main/java/com/unibook/app/dto/request/inventory/CreateInventoryRequest.java
+++ backend/src/main/java/com/unibook/app/dto/request/inventory/CreateInventoryRequest.java
@@ -1,6 +1,8 @@
 package com.unibook.app.dto.request.inventory;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,15 +10,21 @@ import lombok.Setter;
 @Setter
 public class CreateInventoryRequest {    
     
+    @Size(min = 1, max = 10, message = "Sector must contain between 1 and 10 characters")
+    @NotBlank(message = "Sector is required")
     @Schema(example = "A")
     private String sector;
     
+    @Size(min = 1, max = 10, message = "Shelf must contain between 1 and 10 characters")
+    @NotBlank(message = "Shelf is required")
     @Schema(example = "2")
     private String shelf;
     
+    @NotBlank(message = "Row is required")
     @Schema(example = "4")
     private Integer row;
     
+    @NotBlank(message = "Slot is required")
     @Schema(example = "12")
     private Integer slot;
 

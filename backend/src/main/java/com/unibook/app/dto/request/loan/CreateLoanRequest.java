eb09137e7670f80f7cb986c511b0743f885d09bc
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-28 17:55:12 -0300

    Add validations to  CreateLoanRequest

--- backend/src/main/java/com/unibook/app/dto/request/loan/CreateLoanRequest.java
+++ backend/src/main/java/com/unibook/app/dto/request/loan/CreateLoanRequest.java
@@ -3,6 +3,9 @@ package com.unibook.app.dto.request.loan;
 import java.time.LocalDate;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,12 +17,16 @@ import lombok.Setter;
 @AllArgsConstructor
 public class CreateLoanRequest {
 
+    @NotBlank(message = "User is required")
     @Schema(example = "1")
     private Long userId;
 
+    @NotBlank(message = "Copy is required")
     @Schema(example = "1")
     private Long copyId;
 
+    @Future(message = "Due date must be in the future")
+    @NotNull(message = "Due date is required")
     @Schema(example = "2026-05-20")
     private LocalDate dueDate;
 }

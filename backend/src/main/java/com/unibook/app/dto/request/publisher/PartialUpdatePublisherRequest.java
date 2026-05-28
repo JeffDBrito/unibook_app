04149b20cf5e26214173cc60eb92daa1c5365a0d
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-28 19:41:07 -0300

    Create PartialUpdatePublisherRequest

---
+++ backend/src/main/java/com/unibook/app/dto/request/publisher/PartialUpdatePublisherRequest.java
@@ -0,0 +1,17 @@
+package com.unibook.app.dto.request.publisher;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PartialUpdatePublisherRequest {
+    
+    @Schema(example = "Penguin Random House")
+    private String title;
+    
+    @Schema(example = "Penguin Random House is a global book publishing company that publishes a wide range of fiction and non-fiction books across various genres.")
+    private String description;
+
+}

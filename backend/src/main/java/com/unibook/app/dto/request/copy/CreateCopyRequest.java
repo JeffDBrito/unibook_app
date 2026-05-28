26710fcdb6c9fe492f84e131429364d790155309
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-27 22:56:30 -0300

    Add Validation to CreateCopyRequest

--- backend/src/main/java/com/unibook/app/dto/request/copy/CreateCopyRequest.java
+++ backend/src/main/java/com/unibook/app/dto/request/copy/CreateCopyRequest.java
@@ -3,6 +3,9 @@ package com.unibook.app.dto.request.copy;
 import com.unibook.app.enums.CopyStatus;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,12 +19,16 @@ import lombok.ToString;
 @ToString
 public class CreateCopyRequest {    
 
+    @Size(min = 12, max = 12, message = "Code must contain 12 characters")
+    @NotBlank(message = "Code is required")
     @Schema(example = "123AS-ASD123")
     private String code;
 
+    @NotNull(message = "Status is required")
     @Schema(example = "AVAILABLE")
     private CopyStatus status;
 
+    @NotBlank(message = "Book is required")
     @Schema(example = "1")
     private Long bookId;
 

cc895d1bf874b2340a2f424e7d781d133d27514f
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-27 22:28:27 -0300

    Add validation to UpdateCategoryRequest

--- backend/src/main/java/com/unibook/app/dto/request/category/UpdateCategoryRequest.java
+++ backend/src/main/java/com/unibook/app/dto/request/category/UpdateCategoryRequest.java
@@ -1,6 +1,8 @@
 package com.unibook.app.dto.request.category;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +10,13 @@ import lombok.Setter;
 @Setter
 public class UpdateCategoryRequest {
     
+    @Size(min = 1, max = 30, message = "Title must contain between 1 and 30 characters")
+    @NotBlank(message = "Title is required")
     @Schema(example = "Fiction")
     private String title;
-
+ 
+    @Size(min = 1, max = 100, message = "Description must contain between 1 and 100 characters")
+    @NotBlank(message = "Description is required")
     @Schema(example = "Fiction is a literary genre that includes imaginative or invented stories, often featuring characters and events that are not based on real life.")
     private String description;
 

3cb7fa6928312b6f5fda70d0c6d1ccabcedaa04a
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-27 22:28:38 -0300

    Create PartialUpdateCategoryRequest

---
+++ backend/src/main/java/com/unibook/app/dto/request/category/PartialUpdateCategoryRequest.java
@@ -0,0 +1,17 @@
+package com.unibook.app.dto.request.category;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PartialUpdateCategoryRequest {
+    
+    @Schema(example = "Fiction")
+    private String title;
+
+    @Schema(example = "Fiction is a literary genre that includes imaginative or invented stories, often featuring characters and events that are not based on real life.")
+    private String description;
+
+}

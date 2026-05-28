c7c1074aa92ef6964b19f105c3a0561b3d0e1aff
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-05-27 23:02:31 -0300

    Add validation to UpdateCopyRequest

--- backend/src/main/java/com/unibook/app/dto/request/copy/UpdateCopyRequest.java
+++ backend/src/main/java/com/unibook/app/dto/request/copy/UpdateCopyRequest.java
@@ -3,22 +3,32 @@ package com.unibook.app.dto.request.copy;
 import com.unibook.app.enums.CopyStatus;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UpdateCopyRequest {    
-    
-    @Schema(example = "1")
-    private Long id;
 
+    @Size(min = 12, max = 12, message = "Code must contain 12 characters")
+    @NotBlank(message = "Code is required")
     @Schema(example = "123AS-ASD123")
     private String code;
 
+    @NotNull(message = "Status is required")
     @Schema(example = "AVAILABLE")
     private CopyStatus status;
 
+    // Can be null or Long
     @Schema(example = "1")
     private Long inventoryId;
 

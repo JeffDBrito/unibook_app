8f186f410916be90d25c1ce2fc58abe3080cb3e2
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-06-27 21:17:48 -0300

    Add css files and import it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (

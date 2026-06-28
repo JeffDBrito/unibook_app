79d5fc099b15746dda90a2bff4cde2eb57356899
Jefferson Brito <jeffersondanilo2517@gmail.com>
2026-06-27 21:18:09 -0300

    Add sidebar icons

---
+++ frontend/src/components/icons/DashboardIcon.jsx
@@ -0,0 +1,5 @@
+export default function DashboardIcon(){
+    return (
+        <svg width="30" height="30" className="col-12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 5V19C4 19.5523 4.44772 20 5 20H19" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M18 9L13 13.9999L10.5 11.4998L7 14.9998" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+    );
+}
